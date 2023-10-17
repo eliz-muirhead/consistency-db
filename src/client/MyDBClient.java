6631ab63e02ddbd64e6bec661398d2ae69b79610
elizabeth <eliz.muirhead@gmail.com>
2023-10-17 18:19:38 -0400

    add design doc

--- src/client/MyDBClient.java
+++ src/client/MyDBClient.java
@@ -9,6 +9,7 @@ import server.SingleServer;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This class should implement your DB client.
@@ -25,11 +26,13 @@ public class MyDBClient extends Client {
     @Override
     public void callbackSend(InetSocketAddress isa, String request, Callback
             callback){
-        //TODO
+        NIOHeader header = new NIOHeader(isa, isa);
+        callback.handleResponse(request.getBytes(StandardCharsets.UTF_8), header);
     }
 
     @Override
     public void handleResponse(byte[] bytes, NIOHeader header){
-        //TODO
+        String stringResponse = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(stringResponse);
     }
 }
